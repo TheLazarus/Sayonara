0e7367e31448ab8aeb4ed3e486ccc52225b17faf
lazzarus <ss22072000@gmail.com>
2022-07-21 09:48:57 +0530

    feat: Improved Styling

--- components/Torrent/index.tsx
+++ components/Torrent/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { ITorrent } from "../../config/typings";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import AccordionDetails from "@mui/material/AccordionDetails";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import { ITorrent } from "../../config/typings";
 
 type ITorrentProps = {
   torrent: ITorrent;
@@ -12,21 +13,53 @@ type ITorrentProps = {
 
 const Torrent: React.FunctionComponent<ITorrentProps> = ({ torrent }) => {
   return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
+    <Box
+      sx={{
+        width: "90vw",
+        display: "flex",
+        gap: "1em",
+      }}
+    >
+      <Card
+        sx={{
+          flex: 5,
+          backgroundColor: "#FFFCE8",
+          borderRadius: "1em",
+        }}
       >
-        <Typography>{torrent.name}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        {/* <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </Typography> */}
-      </AccordionDetails>
-    </Accordion>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {torrent.category}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {torrent.name}
+          </Typography>
+          <Typography sx={{ mb: 1.5 }} color="text.secondary">
+            {torrent.size}
+          </Typography>
+          <Typography variant="body2">
+            Uploaded By {torrent.uploader} on {torrent.uploadDate}
+            <br />
+          </Typography>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            Seeders - {torrent.seeders} | Leechers - {torrent.leechers}
+          </Typography>
+        </CardContent>
+        {/* <CardActions>
+          <Button size="small">
+            <a href={torrent.magnetLink}>MAGNET</a>
+          </Button>
+        </CardActions> */}
+      </Card>
+      <Card sx={{ flex: 1, borderRadius: "1em", backgroundColor: "#CD5334" }}>
+        <CardContent>MAGNET</CardContent>
+        <CardActions>
+          <Button size="small">
+            <a href={torrent.magnetLink}>MAGNET</a>
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
--- components/TorrentTable/index.tsx
+++ components/TorrentTable/index.tsx
@@ -1,16 +1,7 @@
 import React from "react";
-import Link from "next/link";
 import { ITorrentInformation } from "../../config/typings";
-import Container from "@mui/material/Container";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
 import Torrent from "../Torrent";
-import styles from "./style.module.css";
 
 type ITorrentTableProps = {
   torrents: ITorrentInformation;
@@ -20,47 +11,20 @@ const TorrentTable: React.FunctionComponent<ITorrentTableProps> = ({
   torrents,
 }) => {
   return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: "100vw" }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell align="center" sx={{ position: "relative" }}>
-                Category
-              </TableCell>
-              <TableCell align="center">Name</TableCell>
-              <TableCell align="center">Size</TableCell>
-              <TableCell align="center">Seeders</TableCell>
-              <TableCell align="center">Leechers</TableCell>
-              <TableCell align="center">Upload Date</TableCell>
-              <TableCell align="center">Uploader</TableCell>
-              <TableCell align="center">Magnet</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {torrents.map((torrent) => (
-              <TableRow
-                key={torrent.magnetLink}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell align="center">{torrent.category}</TableCell>
-                <TableCell component="th" scope="torrent">
-                  {torrent.name}
-                </TableCell>
-                <TableCell align="center">{torrent.size}</TableCell>
-                <TableCell align="center">{torrent.seeders}</TableCell>
-                <TableCell align="center">{torrent.leechers}</TableCell>
-                <TableCell align="center">{torrent.uploadDate}</TableCell>
-                <TableCell align="center">{torrent.uploader}</TableCell>
-                <TableCell align="center">
-                  <a href={torrent.magnetLink}>Magnet</a>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </>
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        padding: "1em",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1em",
+      }}
+    >
+      {torrents.map((torrent) => (
+        <Torrent torrent={torrent} key={torrent.magnetLink} />
+      ))}
+    </Box>
   );
 };
 
